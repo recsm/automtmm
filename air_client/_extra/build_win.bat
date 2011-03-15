4da22de0187fec4e99b3b85c7260d8bad882b937
Thomas Gruner <toml.gruner@gmail.com>
2011-03-15 21:14:31 +0100

    Added in putty and plink, worked on cloning, branching

--- air_client/_extra/build_win.bat
+++ air_client/_extra/build_win.bat
@@ -1,1 +1,3 @@
-adt -package -storetype pkcs12 -keystore certificate.pfx -target native mac.exe application.xml main.html lib css icons
+cd ..
+adt -package -tsa none -storetype pkcs12 -keystore certificate.pfx -target native mac.exe application.xml main.html putty lib css icons
+cd _extra
