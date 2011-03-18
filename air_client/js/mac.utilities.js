3213fc054470bcba21caaf21fbd5eae7671c6163
Thomas Gruner <toml.gruner@gmail.com>
2011-03-18 14:02:27 +0100

    Seperated out mac.utilites

---
+++ air_client/js/mac.utilities.js
@@ -0,0 +1,19 @@
+mac.utilities = {
+	//Helper function to get a dom node from a certain tab using a css query
+	getTabNode : function(query, tab) {
+		return dojo.query(query, tab.containerNode)[0]
+	},
+	//Helper function to get a dojo dijit from a certain tab
+	getTabDijit : function(query, tab) {
+		return dijit.byNode(mac.utilities.getTabNode(query, tab))
+	},
+	//Shortcut to hide a node
+	nodeHide : function(node) {
+		dojo.style(node, {"visibility" : "hidden", "position" : "absolute"})
+	},
+	//Shortcut to show a node
+	nodeShow : function(node) {
+		dojo.style(node, {"visibility" : "visible", "position" : "static"})
+	}
+	
+}
